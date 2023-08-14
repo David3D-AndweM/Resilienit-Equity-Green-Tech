928bc8ac0db62ec659e90f43a378e6c7c2fce9ca
David3D-AndweM <127220902+David3D-AndweM@users.noreply.github.com>
2023-08-14 21:37:51 +0300

    Update form-handler.php

--- REGTECH/form-handler.php
+++ REGTECH/form-handler.php
@@ -1,19 +1,46 @@
 <?php
+// form-handler.php
+
+// Check if the form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
     $fullName = $_POST["fullName"];
     $email = $_POST["email"];
     $phone = $_POST["phone"];
     $areaInterest = $_POST["areaInterest"];
     $motivation = $_POST["motivation"];
 
-    $to = "davidmwape376@gmail.com"; // Your email address
-    $subject = "Ambassador Program Registration";
-    $message = "Full Name: " . $fullName . "\n";
-    $message .= "Email: " . $email . "\n";
-    $message .= "Phone Number: " . $phone . "\n";
-    $message .= "Area of Interest: " . $areaInterest . "\n";
-    $message .= "Motivation: " . $motivation . "\n";
+    // Process the data (you can save it to a database or perform other actions)
 
-    mail($to, $subject, $message);
+    // Forward the data to Formspree using cURL
+    $url = "https://formspree.io/f/mrgwponn";
+    $data = array(
+        "fullName" => $fullName,
+        "email" => $email,
+        "phone" => $phone,
+        "areaInterest" => $areaInterest,
+        "motivation" => $motivation
+    );
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        // Error handling
+        echo "Form submission failed: " . curl_error($ch);
+    } else {
+        // Successful submission handling
+        echo "Form submitted successfully";
+    }
+
+    curl_close($ch);
+} else {
+    // Redirect back to the form if accessed directly
+    header("Location: REG-form.html");
+    exit();
 }
 ?>
